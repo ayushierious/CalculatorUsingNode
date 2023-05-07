d491ee7c893aff5965f86b97f96a6d900e6b9a08
ayushierious <1089ayushiraj@gmail.com>
2023-05-08 00:03:07 +0530

    basic calculator

--- calculator.js
+++ calculator.js
@@ -1,11 +1,23 @@
 
 const express=require("express");
+const bodyParser=require("body-parser")
+
 const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+
  app.get("/", function(req,res){
    res.sendFile(__dirname + "/index.html");
  });
  app.post("/", function (req,res){
-   res.send("thanks for posting")
+
+   // console.log(req.body);
+   // res.send("thanks for posting")
+
+   var num1=Number(req.body.num1);
+   var num2=Number(req.body.num2);
+   var result=num1+num2;
+   res.send("The sum is "+result);
+
  })
  app.listen(3000,function(){
    console.log("server started at 3000");
