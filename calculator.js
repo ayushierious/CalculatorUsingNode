12a6f70dda3374a228a91cd96a5edfad5e235cb6
ayushierious <1089ayushiraj@gmail.com>
2023-05-08 23:16:21 +0530

    bmiCalculator

--- calculator.js
+++ calculator.js
@@ -19,6 +19,15 @@ app.use(bodyParser.urlencoded({extended:true}));
    res.send("The sum is "+result);
 
  })
+ app.get("/bmicalculator", function (req,res){
+   res.sendFile(__dirname+"/bmicalculator.html");
+ });
+ app.post("/bmicalculator", function(req, res){
+   var weight=Number(req.body.weight);
+   var height=Number(req.body.height);
+   var bmi=weight/ (height * height);
+   res.send("Your BMI is : "+bmi);
+ });
  app.listen(3000,function(){
    console.log("server started at 3000");
  });
